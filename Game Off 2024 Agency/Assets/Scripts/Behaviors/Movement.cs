15badf0c704b2ec946ef9fff991ca637a9f97e88
GabrielRRG <gabrielramalhorg@gmail.com>
2024-11-04 13:54:31 -0300

    Agent basic setup

---
+++ Game Off 2024 Agency/Assets/Scripts/Behaviors/Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float changeDirectionTime;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(changeDirection());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector3(moveSpeed, 0);
+    }
+    IEnumerator changeDirection(){
+        while (true){
+            yield return new WaitForSeconds(changeDirectionTime);
+            moveSpeed = -moveSpeed;
+        }
+    }
+}
